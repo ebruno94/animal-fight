310f50de3bde64a978e6a1f8d2b2c68041ae3389
Ernest Bruno and Eric Swotinsky <ernest@>
2018-01-09 15:46:09 -0800

    add buttons for additional animals. change list IDs to p1 and p2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
   $("button#greet").click(function(){
-    $("ul#cat").prepend("<li>Meow~!</li>");
-    $("ul#dog").prepend("<li>Arf~!</li>")
+    $("ul#p1").prepend("<li>Meow~!</li>");
+    $("ul#p2").prepend("<li>Arf~!</li>")
   });
 
   $("button#attack").click(function(){
+    $("ul#p1").prepend("<li>RAWR~!</li>");
+    $("ul#p2").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#dog").click(function(){
+    $("ul#cat").prepend("<li>RAWR~!</li>");
+    $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#cat").click(function(){
+    $("ul#cat").prepend("<li>RAWR~!</li>");
+    $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#fox").click(function(){
+    $("ul#cat").prepend("<li>RAWR~!</li>");
+    $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#guppy").click(function(){
+    $("ul#cat").prepend("<li>RAWR~!</li>");
+    $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#trex").click(function(){
+    $("ul#cat").prepend("<li>RAWR~!</li>");
+    $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
+  });
+
+  $("button#walrus").click(function(){
     $("ul#cat").prepend("<li>RAWR~!</li>");
     $("ul#dog").prepend("<li>AWOOOOOO~!</li>");
   });
